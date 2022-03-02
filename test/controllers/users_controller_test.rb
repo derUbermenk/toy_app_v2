69c27ca6eddcdb89dc7e5e96c69d67b9936330b6
Chester Asuncion <asuncionmcv@gmail.com>
2022-03-02 16:37:02 +0800

    add test for users_controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = create :user
+  end
+
+  test 'it should get show' do
+    get user_path(@user.id)
+    assert_response :success
+  end
 end
